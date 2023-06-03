5c44a6d40708c58782c5147b9505405e45a3fdca
ru0ya <viktaangi02@gmail.com>
2023-06-03 22:10:03 +0300

    debug

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -4,8 +4,6 @@ Route module for the API
 """
 from os import getenv
 from api.v1.views import app_views
-#from api.v1.auth.auth import Auth
-#from api.v1.auth.basic_auth import BasicAuth
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
@@ -32,19 +30,21 @@ def before_request():
     method for filtering each request
     """
     if auth is None:
-        pass
+        return
 
     excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
                       '/api/v1/forbidden/']
 
-    if auth.require_auth(request.path, excluded_paths):
-        auth_header = auth.authorization_header(request)
-        if auth_header is None:
-            abort(401, description='Unauthorized')
+    if not (auth.require_auth(request.path, excluded_paths)):
+        return
+    
+    auth_header = auth.authorization_header(request)
+    if auth_header is None:
+        abort(401, description='Unauthorized')
 
-        auth_user = auth.current_user(request)
-        if auth_user is None:
-            abort(403, description='Forbidden')
+    auth_user = auth.current_user(request)
+    if auth_user is None:
+        abort(403, description='Forbidden')
 
 
 @app.errorhandler(404)
