d901096a21632941bde1e9094bb03065936c8e44
ru0ya <viktaangi02@gmail.com>
2023-06-09 22:02:58 +0300

    debug

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -3,7 +3,7 @@
 DB module
 """
 from sqlalchemy import create_engine
-from asqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import session
 from sqlalchemy.orm.exc import NoResultFound
