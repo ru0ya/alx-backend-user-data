8dbed8eac6d96e7f7daa84a1234b4dfd2b62005f
ru0ya <viktaangi02@gmail.com>
2023-06-10 22:18:05 +0300

    debug

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -28,11 +28,11 @@ class SessionAuth(Auth):
         if user_id is None or not isinstance(user_id, str):
             return None
 
-        id = uuid.uuid4()
+        session_id = str(uuid.uuid4())
 
-        self.user_id_by_session_id[id] = user_id
+        self.user_id_by_session_id[session_id] = user_id
 
-        return id
+        return session_id
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
         """
