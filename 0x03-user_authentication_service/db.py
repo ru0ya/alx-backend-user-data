c34291f6a2d331f309112011ef8738b603323e29
ru0ya <viktaangi02@gmail.com>
2023-06-11 21:29:22 +0300

    debug

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,11 +5,11 @@ DB module
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm.session import session
+from sqlalchemy.orm.session import Session
 from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.exc import InvalidRequestError
 
-from user import Base
+from user import Base, User
 
 
 class DB:
